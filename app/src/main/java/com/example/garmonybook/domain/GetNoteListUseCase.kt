4bb610ded3e050c9a0ce901d34188d2452c3dcc4
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-11-10 16:34:31 +0300

    create usecase

---
+++ app/src/main/java/com/example/garmonybook/domain/GetNoteListUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.garmonybook.domain
+
+class GetNoteListUseCase {
+
+    fun getNoteList(): List<NoteItem>{
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/garmonybook/domain/NoteItem.kt
@@ -0,0 +1,9 @@
+package com.example.garmonybook.domain
+
+data class NoteItem (
+    val id: Int,
+    val name: String,
+    val description: String,
+    val score: Int
+        ) {
+}
