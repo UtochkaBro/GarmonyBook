cd3af612e0d66b9eea04983f1655e73373db8038
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-11-12 10:26:27 +0300

    decided problem with compilator, to lower version appcombat on 1 2 0

--- app/src/main/java/com/example/garmonybook/presentation/MainActivity.kt
+++ app/src/main/java/com/example/garmonybook/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.garmonybook.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.garmonybook.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.noteList.observe(this){
+            Log.d("MainActivityTest", it.toString())
+        }
+
+        viewModel.getNoteList()
     }
 }
